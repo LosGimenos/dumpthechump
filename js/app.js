c2bb61a76ab45da8c0c53edf473e5cf4be03c4a3
Larry Hernandez <larryhernanjr@gmail.com>
2016-09-01 15:35:28 -0400

    add JS logic for storage of moves, body part selection and execution of animates.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
+window.addEventListener('keydown', checkKeyPressed, false);
 
+const keyInputs = [81, 87, 69];
+const moves = ['shake', 'bump', 'nene'];
+const hotMoves = ['move'];
+const grabParts = document.querySelectorAll('.part');
+
+function keyInputsToMoves(keyPressed) {
+  for (let i = 0; i < keyInputs.length; i++) {
+    if (keyPressed.keyCode === keyInputs[i]) {
+      return moves[i];
+    }
+  }
+}
+
+function checkKeyPressed(e) {
+  const findActive = document.getSelection(e).focusNode.id;
+  for (let i = 0; i < grabParts.length; i++) {
+    if (grabParts[i].id === findActive) {
+      grabParts[i].className = 'part';
+      grabParts[i].className += ' ' + keyInputsToMoves(e);
+    }
+  }
+}
